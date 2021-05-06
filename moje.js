d4c75b8714c8d66e608ca9c4bd43027ac07f3b11
Paulina <45172772+Berghauzen@users.noreply.github.com>
2021-05-06 06:12:20 +0200

    Add files via upload

---
+++ moje.js
@@ -0,0 +1,11 @@
+function wypiszTekst(tekst)
+{
+    alert(tekst + "!!!!!!!!!!!");
+}
+function wypiszCosTam()
+{
+    alert("ajaja");
+}
+var test = document.getElementById("test");
+
+test.onclick = wypiszCosTam;
